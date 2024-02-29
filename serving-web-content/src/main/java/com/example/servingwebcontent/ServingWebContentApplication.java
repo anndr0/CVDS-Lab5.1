f07ae0289e682325cd7b6d34f5175bff616a0a89
anndr0 <ana.duran-b@mail.escuelaing.edu.co>
2024-02-29 17:44:50 -0500

    punto controlador springboot

--- serving-web-content/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ serving-web-content/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -2,10 +2,15 @@ package com.example.servingwebcontent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @SpringBootApplication
 public class ServingWebContentApplication {
-
+	@RequestMapping("/")
+	public String home() {
+		return "Hello world Spring Boot";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServingWebContentApplication.class, args);
 	}
