b5cfcc2f0b85baa96940f301c5312f43aa27e215
unknown <ana.duran-b@labinfo.is.escuelaing.edu.co>
2024-03-07 18:08:09 -0500

    conectado a react

--- serving-web-content/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ serving-web-content/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -1,17 +1,30 @@
 package com.example.servingwebcontent;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @SpringBootApplication
 public class ServingWebContentApplication {
+
 	@RequestMapping("/")
 	public String home() {
 		return "Hello world Spring Boot";
 	}
+
+	@RequestMapping("/api/hello")
+	public String hello() {
+		return "Integrantes: Ana Duran, Johan Estrada, Juan Contreras";
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServingWebContentApplication.class, args);
 	}
 }
+
